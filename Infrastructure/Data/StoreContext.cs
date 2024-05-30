b6ed80fc5343b118084d5b4ae4ced7cce48548d3
Sergei Aleshchenko <serg.aleshchenko@gmail.com>
2024-05-31 00:24:34 +0400

    Learning

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+    
+    
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseSqlite("DefaultConnection");
+    //     base.OnConfiguring(optionsBuilder);
+    // }
+    
+    public DbSet<Product> Products { get; set; }
+    
+}
