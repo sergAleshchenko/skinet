1f914dc619385f7b7f63d748712ca8c99b26c9ef
Sergei Aleshchenko <serg.aleshchenko@gmail.com>
2024-06-17 01:35:54 +0400

    Learning

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -4,7 +4,9 @@ namespace Core.Interfaces;
 
 public interface IProductRepository
 {
-    Task<Product> GetProductByIdAsync(int id);
+    Task<Product?> GetProductByIdAsync(int id);
     
     Task<IReadOnlyList<Product>> GetProductsAsync();
+    Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+    Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
 }
